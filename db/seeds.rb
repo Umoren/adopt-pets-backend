6436a2d393354dbd61e13eb30ec8168be7901fef
Umoren <samuelumoren365@gmail.com>
2022-03-30 09:05:18 +0100

    My first commit??

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+Pet.destroy_all 
+Pet.reset_pk_sequence
+
+
+30.times do
+    Pet.create(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, age:Faker::Creature::Dog.age, location: 
+    "NYC", img:"")
+end
+
+
+Pet.all.each do |pet| 
+    pet_image = RestClient.get('https://dog.ceo/api/breeds/image/random')
+    pet_message = JSON.parse(pet_image)["message"]
+    pet.update(
+        img: pet_message
+    ) 
+end
+
---
+++ serializers/adoption_serializer.rb
@@ -0,0 +1,7 @@
+class AdoptionSerializer < ActiveModel::Serializer
+    attributes :id, :user, :pet
+  
+    belongs_to :user 
+    belongs_to :pet
+end
+  
